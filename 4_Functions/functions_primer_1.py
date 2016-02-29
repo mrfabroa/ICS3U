14f86a4a8a605da99010c2c305d369fa9b591b73
mrfabroa <eric.fabroa@ycdsbk12.ca>
2016-02-28 20:57:58 -0500

    add code for functions intro lesson

---
+++ 4_Functions/functions_primer_1.py
@@ -0,0 +1,26 @@
+__author__ = 'eric'
+"""
+Filename:       functions_primer_1.py
+Description:    A short program to demonstrate the usage if built-in python functions and the math library
+"""
+
+import math
+
+# usage of a built-in function abs()
+number_1 = input("Enter a number: ")
+abs_value = abs(number_1)
+print "The absolute value of", number_1, "is", abs_value
+
+
+# usage of a function from another library (math)
+number =2 input("Enter a number:  ")
+print "The square root of", number_2, "is", math.sqrt(number_2)
+
+
+# usage of built-in function len()
+name = raw_input("Enter your name:  ")
+num_char = len(name)
+print "There are", num_char,"in your name."
+
+
+
