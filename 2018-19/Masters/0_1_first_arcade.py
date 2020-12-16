a546c5ff1a7d073871b9f7dd4ed3a111c9c98cd3
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2020-12-16 07:56:08 -0500

    Update 0_1_first_arcade.py

--- 2018-19/Masters/0_1_first_arcade.py
+++ 2018-19/Masters/0_1_first_arcade.py
@@ -1,23 +1,22 @@
 
-# ?
+# Import necessary libraries
 import arcade
 import os
 
-# ?
+# Define a window space
 arcade.open_window(600, 600, "My First Arcade Program")
 
-# ?
+# Set background colour
 arcade.set_background_color(arcade.color.WHITE)
 
-# ?
+# Drawing occurs after this line
 arcade.start_render()
 
-# ?
+# Examples of drawing commands
 arcade.draw_text("draw_circle_filled", 363, 207, arcade.color.BLACK, 10)
 arcade.draw_circle_filled(420, 285, 18, arcade.color.GREEN)
 
-# ?
+# Drawing occurs before this line
 arcade.finish_render()
 
-# ?
 arcade.run()
